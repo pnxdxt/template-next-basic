0d1e620fe14f2363b031de0e877ea7e3df8499df
pnxdxt <paul.nodet@gmail.com>
2022-02-22 19:00:36 +0100

    Clean

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import Head from 'next/head';
+import type {ReactElement, ReactNode} from 'react';
+import type {NextPage} from 'next';
+import type {AppProps} from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+type NextPageWithLayout = NextPage & {
+	getLayout?: (page: ReactElement) => ReactNode;
+};
 
-export default MyApp
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout;
+};
+
+const customApp = ({
+	Component,
+	pageProps: {session, ...pageProps},
+}: AppPropsWithLayout) => {
+	const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
+
+	return getLayout(
+		<>
+			<Head>
+				{/* Meta properties */}
+				<meta property='og:title' content='title' />
+				<meta name='description' content='description' />
+				<meta name='robots' content='index, no-follow' />
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+			</Head>
+			<Component {...pageProps} />
+		</>,
+	);
+};
+
+export default customApp;
