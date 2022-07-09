76884fe4d172cc01cbbd2f1eb883bf98554e9891
pnxdxt <paul.nodet@gmail.com>
2022-07-09 13:48:38 +0200

    Update template for ease of use

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import type {NextPage} from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 
 const Home: NextPage = () => {
 	return (
 		<div className='flex min-h-screen flex-col items-center justify-center py-2'>
 			<Head>
 				<title>Create Next App</title>
-				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
 			<main className='flex w-full flex-1 flex-col items-center justify-center px-20 text-center'>
@@ -68,15 +66,14 @@ const Home: NextPage = () => {
 				</div>
 			</main>
 
-			<footer className='flex h-24 w-full items-center justify-center border-t'>
+			<footer className='flex h-24 w-full items-center justify-center'>
 				<a
 					className='flex items-center justify-center gap-2'
-					href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+					href='https://github.com/pnxdxt'
 					target='_blank'
 					rel='noopener noreferrer'
 				>
-					Powered by{' '}
-					<Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
+					Made with 🖤 by <b>pnxdxt</b>
 				</a>
 			</footer>
 		</div>
